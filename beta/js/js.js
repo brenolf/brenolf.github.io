77d03c0496712ad814a23390a697886ecb54881d
Breno <breno@Mateuss-MacBook-Air.local>
2014-12-11 02:07:56 -0500

    Changes in beta version (first frame)

--- beta/js/js.js
+++ beta/js/js.js
@@ -1,5 +1,11 @@
 var listener = new window.keypress.Listener();
 
-listener.sequence_combo("up up down down left right left right b a enter", function() {
-	// Make something fun here!
+listener.sequence_combo('up up down down left right left right b a enter', function() {
+	// Make something funky here!
+});
+
+$(document).ready(function(){
+	$('section').snapPoint({
+		scrollSpeed: 200
+	});
 });
