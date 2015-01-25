a4b9678768eabd3c8066d37c12179b466ed6e236
Breno Lima de Freitas <brenolimadefreitas@gmail.com>
2015-01-25 01:40:54 -0200

    Added easter egg

--- beta/js/js.js
+++ beta/js/js.js
@@ -1,10 +1,14 @@
 var listener = new window.keypress.Listener();
 
+var phrases = ['P. Sherman, 42, Sidney.', 'Zhu Li, do the thing!', 'To infinity, and beyond!', 'I like to move it, move it', '42', 'Gotta catch\'em all!'];
+
 listener.sequence_combo('up up down down left right left right b a enter', function() {
 	// Make something funky here!
 });
 
 $(document).ready(function(){
+	$('#postit').text(phrases[~~(Math.random() * phrases.length)]);
+
 	// $('section').snapPoint({
 	// 	scrollSpeed: 150
 	// });
@@ -15,6 +19,6 @@ $(document).ready(function(){
 	// });
 });
 
-window.sr = new scrollReveal({
+//window.sr = new scrollReveal({
 	// reset : true
-});
+//});
