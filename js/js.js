41c110b11cf28ee79c7a9ab9411e95e03a5805e4
Breno Lima de Freitas <brenolimadefreitas@gmail.com>
2015-01-25 19:48:11 -0200

    Updated CV and new website

--- js/js.js
+++ js/js.js
@@ -1,10 +1,58 @@
 var listener = new window.keypress.Listener();
+var current = 0, size = 1, loader = 0;
 
-listener.sequence_combo("up up down down left right left right b a enter", function() {
-	$('#plane-one').css({left: '-1em', bottom: '-1em'});
-	$('#plane-one').removeClass('plane-animation');
+var phrases = ['P. Sherman, 42, Sidney.', 'Zhu Li, do the thing!', 'To infinity, and beyond!', 'I like to move it, move it', 'The answer is 42', 'Gotta catch\'em all!', 'Brain, what you wanna do tonight?', 'Go, Appa! Yip yip!', 'Azarath metrion zinthos', 'Evaaa, WALL-EEEEE', 'Luke, I am your father'];
 
-	setTimeout(function(){
-		$('#plane-one').addClass('plane-animation');
-	}, 5);
+var colours = [['#315714', '#60a928'], ['#172b62', '#5578d7'], ['#000000', '#a5d302'], ['#363435', '#FF6600']];
+
+listener.sequence_combo('up up down down left right left right b a enter', function() {
+	// Make something funky here!
+});
+
+function mod (n, m) {
+	return ((n % m) + m) % m;
+}
+
+function load () {
+	$('.loader').animate({width: '100%'}, 7000, function(){
+		$('.loader').css({width: '0px'});
+		$('.arrow.right').click();
+	});
+}
+
+$(document).ready(function(){
+	$('#postit').text(phrases[~~(Math.random() * phrases.length)]);
+
+	size = $('.panel > div').length;
+
+	$('.arrow.left').click(function(){
+		$('#prj-' + current).fadeOut('slow', function(){
+			current = mod(current - 1, size);
+			$('#prj-' + current).fadeIn('slow');
+
+			$('.block-4 h1').css({color: colours[current][0]});
+			$('.block-4').css({background: colours[current][1]});
+		});
+	});
+
+	$('.arrow.right').click(function(){
+		$('#prj-' + current).fadeOut('slow', function(){
+			current = mod(current + 1, size);
+			$('#prj-' + current).fadeIn('slow');
+
+			$('.block-4 h1').css({color: colours[current][0]});
+			$('.block-4').css({background: colours[current][1]});
+		});
+	});
+	
+	$('section').snapPoint({
+		scrollSpeed: 150
+	});
+
+	setInterval(load, 7005);
+
+	// $('#fullpage').fullpage({
+	// 	verticalCentered: false,
+	// 	scrollingSpeed: 400
+	// });
 });
