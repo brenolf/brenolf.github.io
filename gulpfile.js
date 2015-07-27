03f07006a67231aab4069aa31bac8a97d8720ed3
Breno Lima de Freitas <brenolimadefreitas@gmail.com>
2015-07-27 16:04:26 -0300

    Refactor code

--- gulpfile.js
+++ gulpfile.js
@@ -4,45 +4,80 @@ var gulp        = require('gulp'),
     prefixer    = require('gulp-autoprefixer'),
     rename      = require('gulp-rename'),
     concat      = require('gulp-concat'),
-    uglify      = require('gulp-uglify');
+    img         = require('gulp-imagemin'),
+    uglify      = require('gulp-uglify'),
+    ghPages     = require('gulp-gh-pages-cname'),
+    copy        = require('gulp-copy'),
+    htmlmin     = require('gulp-html-minify'),
+    del         = require('del');
 
-gulp.task ('default', ['styles', 'scripts']);
-gulp.task ('all', ['default', 'cv', 'audience']);
+var assets = './build/assets';
 
-gulp.task ('scripts', function () {
-    return gulp
-    .src('./js/breno.io.js')
-    .pipe(uglify())
-    .pipe(rename('breno.io.min.js'))
-    .pipe(gulp.dest('./js'));
+gulp.task ('default', ['styles', 'scripts', 'img', 'copy-pdf', 'html']);
+
+gulp.task ('scripts', ['vendor-scripts'], function () {
+  return gulp
+  .src('./js/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest(assets + '/js'));
 });
 
-gulp.task ('styles', function () {
-    return gulp
-    .src('./css/**/*.less')
-    .pipe(less())
-    .pipe(prefixer())
-    .pipe(minifyCSS())
-    .pipe(concat('css.css'))
-    .pipe(gulp.dest('./css'));
+gulp.task ('vendor-scripts', function () {
+  return gulp
+  .src('./js/vendor/*')
+  .pipe(gulp.dest(assets + '/js/vendor'));
 });
 
-gulp.task ('cv', function () {
-    return gulp
-    .src('./cv/css/*.less')
-    .pipe(less())
-    .pipe(prefixer())
-    .pipe(minifyCSS())
-    .pipe(concat('css.css'))
-    .pipe(gulp.dest('./cv/css'));
+gulp.task ('vendor-styles', function () {
+  return gulp
+  .src('./style/vendor/*')
+  .pipe(gulp.dest(assets + '/css/vendor'));
 });
 
-gulp.task ('audience', function () {
-    return gulp
-    .src('./audience/less/*.less')
-    .pipe(less())
-    .pipe(prefixer())
-    .pipe(minifyCSS())
-    .pipe(concat('css.css'))
-    .pipe(gulp.dest('./audience/assets/css'));
+gulp.task ('styles', ['vendor-styles', 'copy-fonts'], function () {
+  return gulp
+  .src('./style/*.less')
+  .pipe(less())
+  .pipe(prefixer())
+  .pipe(minifyCSS())
+  .pipe(gulp.dest(assets + '/css'));
+});
+
+gulp.task ('html', function () {
+  return gulp
+  .src('./html/**/*.html')
+  .pipe(htmlmin())
+  .pipe(gulp.dest('./build'));
+});
+
+gulp.task ('img', function() {
+  return gulp
+  .src('./img/*')
+  .pipe(img())
+  .pipe(gulp.dest(assets + '/img'));
+});
+
+gulp.task ('copy-pdf', function () {
+  return gulp
+  .src('./pdf/*')
+  .pipe(gulp.dest(assets + '/pdf'));
+});
+
+gulp.task ('copy-fonts', function () {
+  return gulp
+  .src('./font/*')
+  .pipe(gulp.dest(assets + '/font'));
+});
+
+gulp.task ('clean', function () {
+  del(['./build', './*.publish']);
+});
+
+gulp.task ('deploy', ['default'], function() {
+  return gulp
+  .src('./build/**/*')
+  .pipe(ghPages({
+    branch: 'master',
+    cname: 'breno.io'
+  }));
 });
--- js/breno.io.js
+++ js/breno.io.js
@@ -5,123 +5,140 @@ var transitioning = false;
 var modifier = '';
 var last = -1;
 
-var phrases = ['P. Sherman, 42, Sidney.', 'Zhu Li, do the thing!', 'To infinity, and beyond!', 'I like to move it, move it', 'The answer is 42', 'Gotta catch\'em all!', 'Brain, what you wanna do tonight?', 'Go, Appa! Yip yip!', 'Azarath metrion zinthos', 'Evaaa, WALL-EEEEE', 'Luke, I am your father'];
+var phrases = [
+  'P. Sherman, 42, Sidney.',
+  'Zhu Li, do the thing!',
+  'To infinity, and beyond!',
+  'I like to move it, move it',
+  'The answer is 42',
+  'Gotta catch\'em all!',
+  'Brain, what you wanna do tonight?',
+  'Go, Appa! Yip yip!',
+  'Azarath metrion zinthos',
+  'Evaaa, WALL-EEEEE',
+  'Luke, I am your father'
+];
 
-var colours = [['#000000', '#E7E737'], ['#324F36', '#60a928'], ['#172b62', '#5578d7'], ['#000000', '#a5d302']];
+var colours = [
+  ['#000000', '#E7E737'],
+  ['#324F36', '#60a928'],
+  ['#172b62', '#5578d7'],
+  ['#000000', '#a5d302']
+];
 
 // listener.sequence_combo('up up down down left right left right b a enter', function() {
 //     // Make something funky here!
 // });
 
 function mod (n, m) {
-    return ((n % m) + m) % m;
+  return ((n % m) + m) % m;
 }
 
 function load () {
-    modifier = $('.smaller').css('display') === 'block' ? 's' : '';
+  modifier = $('.smaller').css('display') === 'block' ? 's' : '';
 
-    if (transitioning) {
-        timer = setTimeout(load, speed + 5);
-        return;
-    }
+  if (transitioning) {
+    timer = setTimeout(load, speed + 5);
+    return;
+  }
 
-    $('#bar').animate({width: '100%'}, speed, function(){
-        $('#bar').css('width', '0');
-        slide(+1);
-    });
+  $('#bar').animate({width: '100%'}, speed, function(){
+    $('#bar').css('width', '0');
+    slide(+1);
+  });
 }
 
 function change_postit () {
-    var die = last;
+  var die = last;
 
-    while (die === last) {
-        die = ~~(Math.random() * phrases.length);
-    }
+  while (die === last) {
+    die = ~~(Math.random() * phrases.length);
+  }
 
-    last = die;
+  last = die;
 
-    $('#postit').animate({top: '+=3%', opacity: 0}, 'fast', function () {
-        $('#postit').text(phrases[die]);
-        $('#postit').css('top', '77%').animate({top: '+=3%', opacity: 1}, 'fast');
-    });
+  $('#postit').animate({top: '+=3%', opacity: 0}, 'fast', function () {
+    $('#postit').text(phrases[die]);
+    $('#postit').css('top', '77%').animate({top: '+=3%', opacity: 1}, 'fast');
+  });
 }
 
 function slide (n) {
-    if (transitioning)
-        return;
+  if (transitioning) {
+    return;
+  }
 
-    transitioning = true;
+  transitioning = true;
 
-    clearTimeout(timer);
-    $('#bar').stop().css('width', '0');
+  clearTimeout(timer);
+  $('#bar').stop().css('width', '0');
 
-    $('#prj-' + current + modifier).fadeOut('slow', function(){
-        current = mod(current + n, size);
-
-        $('.block-4 h1').css('color', colours[current][0]);
-        $('.block-4, .block-4 b').css('background', colours[current][1]);
-
-        $('#prj-' + current + modifier).fadeIn('slow', function () {
-            transitioning = false;
-            timer = setTimeout(load, speed + 5);
-        });
-    });
-}
-
-$(document).ready(function(){
-
-    function loop () {
-        $('.loader span')
-            .animate({opacity: 0.5}, 'slow')
-            .animate({opacity: 1}, 'slow', loop);
-    }
-
-    loop();
-
-    size = $('.panel > div').length;
+  $('#prj-' + current + modifier).fadeOut('slow', function(){
+    current = mod(current + n, size);
 
     $('.block-4 h1').css('color', colours[current][0]);
     $('.block-4, .block-4 b').css('background', colours[current][1]);
 
-    $('.left').click(function(){
-        slide(-1);
-    });
-
-    $('.right').click(function(){
-        slide(+1);
-    });
-
-    window.addEventListener('focus', function() {
-        timer = setInterval(load, speed + 5);
-    });
-
-    window.addEventListener('blur', function() {
-        clearTimeout(timer);
+    $('#prj-' + current + modifier).fadeIn('slow', function () {
         transitioning = false;
-        $('#bar').stop().css('width', '0');
+        timer = setTimeout(load, speed + 5);
     });
+  });
+}
 
-    $('#postit').click(change_postit);
-    change_postit();
+$(document).ready(function(){
+  function loop () {
+    $('.loader span')
+        .animate({opacity: 0.5}, 'slow')
+        .animate({opacity: 1}, 'slow', loop);
+  }
 
-    $('#fullpage').fullpage({
-        verticalCentered: false,
-        resize: false,
-        loopBottom: false,
-        anchors: ['hello', 'me', 'services', 'awards', 'work', 'contact'],
-        menu: '#menu',
-        responsive: 709,
+  loop();
 
-        onLeave: function(index, nextIndex, direction) {
-            $('#menu li:nth-child(' + index + ') i').removeClass().addClass('icon-circle-thin');
-            $('#menu li:nth-child(' + nextIndex + ') i').removeClass().addClass('icon-circle');
-        },
+  size = $('.panel > div').length;
 
-        afterRender : function () {
-            load();
+  $('.block-4 h1').css('color', colours[current][0]);
+  $('.block-4, .block-4 b').css('background', colours[current][1]);
 
-            $('#mask').fadeOut();
-            $('.loader span').stop().stop();
-        }
-    });
+  $('.left').click(function(){
+    slide(-1);
+  });
+
+  $('.right').click(function(){
+    slide(+1);
+  });
+
+  window.addEventListener('focus', function() {
+    timer = setInterval(load, speed + 5);
+  });
+
+  window.addEventListener('blur', function() {
+    clearTimeout(timer);
+    transitioning = false;
+    $('#bar').stop().css('width', '0');
+  });
+
+  $('#postit').click(change_postit);
+  change_postit();
+
+  $('#fullpage').fullpage({
+    verticalCentered: false,
+    resize: false,
+    loopBottom: false,
+    anchors: ['hello', 'me', 'services', 'awards', 'work', 'contact'],
+    menu: '#menu',
+    responsive: 709,
+
+    onLeave: function(index, nextIndex, direction) {
+      $('#menu li:nth-child(' + index + ') i').removeClass().addClass('icon-circle-thin');
+      $('#menu li:nth-child(' + nextIndex + ') i').removeClass().addClass('icon-circle');
+    },
+
+    afterRender : function () {
+      load();
+
+      $('#mask').fadeOut();
+      $('.loader span').stop().stop();
+    }
+  });
 });
